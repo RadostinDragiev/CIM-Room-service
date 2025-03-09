13e24095d8f494e51862153d782b3f3f0b306368
Radostin Dragiev <56969624+RadostinDragiev@users.noreply.github.com>
2025-03-09 19:39:07 +0200

    Initial commit

---
+++ src/main/java/com/cim/roomservice/CimRoomServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cim.roomservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CimRoomServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CimRoomServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cim/roomservice/CimRoomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cim.roomservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CimRoomServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
